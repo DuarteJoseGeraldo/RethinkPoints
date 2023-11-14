3b5e0c0e9070286116308aaf1b9d3515596e5842
DuarteJoseGeraldo <124914532+DuarteJoseGeraldo@users.noreply.github.com>
2023-11-14 10:19:18 -0300

    first commit

---
+++ src/main/java/com/example/hogwartsPoints/HogwartsPointsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hogwartsPoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsPointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsPointsApplication.class, args);
+	}
+
+}
